87a584f7c7969dd215f8d02f181deef39cf35176
jiehong jiang <jiehongjiang@jiehongs-MacBook-Pro.local>
2017-07-25 11:31:37 +0800

    create GameObject

---
+++ GameObject/GameObject.cpp
@@ -0,0 +1,9 @@
+//
+//  GameObject.cpp
+//  switch
+//
+//  Created by jiehong jiang on 7/25/17.
+//
+//
+
+#include "GameObject.hpp"
---
+++ GameObject/GameObject.hpp
@@ -0,0 +1,19 @@
+//
+//  GameObject.hpp
+//  switch
+//
+//  Created by jiehong jiang on 7/25/17.
+//
+//
+
+#ifndef GameObject_hpp
+#define GameObject_hpp
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+class GameObject : Sprite{
+    
+};
+
+#endif /* GameObject_hpp */
