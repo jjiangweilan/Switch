e8d01567825f0852e5be5017ee4126fb398745d6
jiehong jiang <jiehongjiang@jiehongs-MacBook-Pro.local>
2017-07-25 11:26:46 +0800

    create GameObject

---
+++ GameObject/GameObject.hpp
@@ -0,0 +1,14 @@
+//
+//  GameObject.hpp
+//  switch
+//
+//  Created by jiehong jiang on 7/25/17.
+//
+//
+
+#ifndef GameObject_hpp
+#define GameObject_hpp
+
+#include <stdio.h>
+
+#endif /* GameObject_hpp */
