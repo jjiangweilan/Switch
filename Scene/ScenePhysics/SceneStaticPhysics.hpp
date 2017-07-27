18a6bde46eec9032504823020381afe5af59e058
jiehong jiang <jiehongjiang@jiehongs-MacBook-Pro.local>
2017-07-27 22:34:32 +0700

    implementing SceneStaticPhysics and BaseScene

---
+++ Scene/ScenePhysics/SceneStaticPhysics.hpp
@@ -0,0 +1,14 @@
+//
+//  ScenePhysics.hpp
+//  switch
+//
+//  Created by jiehong jiang on 7/27/17.
+//
+//
+
+#ifndef SceneStaticPhysics_hpp
+#define SceneStaticPhysics_hpp
+
+#include <stdio.h>
+
+#endif /*SceneStaticPhysics_hpp */
