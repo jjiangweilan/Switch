4017d094081781c1d29348d082f6a43cf3afd396
jiehong jiang <jiehongjiang@jiehongs-MacBook-Pro.local>
2017-08-01 08:20:18 +0800

    implement scenePhysics class to create physics outline from tiled map

--- Scene/BaseScene.cpp
+++ Scene/BaseScene.cpp
@@ -7,15 +7,16 @@
 //
 
 #include "BaseScene.hpp"
+#include "GLES-Render.h"
 
-BaseScene::BaseScene() : scenePhysics_(NULL), gameObjects_(){};
+BaseScene::BaseScene() : scenePhysics_(NULL), gameObjects_(), tiledMap(NULL){};
 BaseScene::~BaseScene(){
     if(scenePhysics_) delete scenePhysics_;
 };
 
-BaseScene* BaseScene::create(const b2Vec2& gravity, BaseContactListener* listener){
+BaseScene* BaseScene::create(TMXTiledMap* map, const b2Vec2& gravity, BaseContactListener* listener){
     auto ptr = new BaseScene();
-    if (ptr->init(gravity, listener) && ptr) {
+    if (ptr->init(map, gravity, listener) && ptr) {
         ptr->autorelease();
         return ptr;
     }
@@ -24,9 +25,20 @@ BaseScene* BaseScene::create(const b2Vec2& gravity, BaseContactListener* listene
     return ptr;
 }
 
-bool BaseScene::init(const b2Vec2& gravity, BaseContactListener* listener){
+bool BaseScene::init(TMXTiledMap* map, const b2Vec2& gravity, BaseContactListener* listener){
     if (Scene::init()){
         scenePhysics_ = new ScenePhysics(gravity, listener);
+        tiledMap = map;
+        
+        Vec2 origin = Director::getInstance()->getVisibleOrigin();
+        map->setPosition(origin);
+        addChild(tiledMap);
+        scenePhysics_->createMapPhysicsOutline(map);
+        
+        GLESDebugDraw* debug = new GLESDebugDraw(PTM_RATIO);
+        scenePhysics_->getWorld()->SetDebugDraw(debug);
+        debug->SetFlags(debug->e_shapeBit | debug->e_pairBit);
+        
         return true;
     }
     
@@ -34,6 +46,7 @@ bool BaseScene::init(const b2Vec2& gravity, BaseContactListener* listener){
 }
 
 void BaseScene::update(float delta){
+    scenePhysics_->step();
     for (auto object : gameObjects_){
         object->update();
     }
---
+++ Scene/DesignedScene/FirstLevel/FirstScene.hpp
@@ -0,0 +1,16 @@
+//
+//  FirstLevel.hpp
+//  switch
+//
+//  Created by jiehong jiang on 7/31/17.
+//
+//
+
+#ifndef FirstScene_hpp
+#define FirstScene_hpp
+#include "BaseScene.hpp"
+class FirstScene : public BaseScene {
+    
+};
+
+#endif /* FirstScene_hpp */
